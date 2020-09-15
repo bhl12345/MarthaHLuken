852fc47205319c82deecc1302b30f7475a759add
bhl12345 <45795278+bhl12345@users.noreply.github.com>
2020-09-15 17:59:57 -0400

    added text to header

--- src/components/About.js
+++ src/components/About.js
@@ -30,7 +30,7 @@ export default class About extends Component {
             <div className="row">
             <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
                 <span className="heading-meta"></span>
-                <h2 className="colorlib-heading"></h2>
+                <h2 className="colorlib-heading">Text</h2>
             </div>
             </div>
             <div className="row row-pt-md">
